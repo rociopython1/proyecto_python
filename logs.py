7dde2b0b46e4113064813b4edefa2515f310ba26
zabsamuel21-star <zabsamuel21@gmail.com>
2026-05-06 10:04:15 -0500

    Update logs.py
    [Samuel Zabala] Actualización: Sistema de logs profesional con fecha y hora.

--- logs.py
+++ logs.py
@@ -1,6 +1,13 @@
-# Registrar errores del sistema en un archivo de texto
-def registrar_log(mensaje):
-    with open("logs.txt", "a") as archivo:
-        archivo.write(mensaje + "\n")
-        
+from datetime import datetime
+
+def registrar_log(nivel, mensaje, modulo):
+    """
+    Registro avanzado de eventos para Software FJ.
+    Niveles: 'INFO', 'WARNING', 'ERROR'
+    """
+    ahora = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    linea = f"[{ahora}] [{nivel}] [Modulo: {modulo}] - {mensaje}\n"
+    
+    with open("logs.txt", "a", encoding="utf-8") as archivo:
+        archivo.write(linea)
         
