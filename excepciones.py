161d543ddaa32fae4d50b84c2b7a3cdc759bd49d
zabsamuel21-star <zabsamuel21@gmail.com>
2026-05-06 10:03:02 -0500

    Update excepciones.py
    Samuel Zabala] Actualización: Excepciones avanzadas para validación de datos.

--- excepciones.py
+++ excepciones.py
@@ -1,11 +1,17 @@
 class ErrorSistema(Exception):
+    """Clase base para todos los errores del Software FJ."""
+    pass
+
+class ErrorDeFormato(ErrorSistema):
+    """Para cuando meten letras en campos de números."""
+    pass
+
+class ErrorDeRango(ErrorSistema):
+    """Para valores numéricos imposibles (ej. -10 horas)."""
     pass
 
 class ClienteInvalidoError(ErrorSistema):
     pass
 
 class ServicioNoDisponibleError(ErrorSistema):
-    pass
-
-class ReservaError(ErrorSistema):
     pass
