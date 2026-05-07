baf714fd51fe3f8e471e936307763260f0f674dc
Rocio <rocioarredondogalviz110@gmail.com>
2026-05-07 18:14:13 -0500

    Mejoras en cliente y pruebas completas

--- clientes.py
+++ clientes.py
@@ -3,20 +3,24 @@ from logs import registrar_log
 
 
 class Cliente:
-    def __init__(self, nombre, correo):
+    def __init__(self, nombre, correo, telefono):
         try:
             if not nombre or len(nombre.strip()) < 2:
                 raise ClienteInvalidoError("El nombre del cliente no es válido")
 
             if not correo or "@" not in correo:
                 raise ClienteInvalidoError("El correo del cliente no es válido")
+            
+            if not telefono.isdigit() or len(telefono) < 10:
+               raise ClienteInvalidoError("El teléfono del cliente no es válido")
 
             # Encapsulación
             self.__nombre = nombre.strip()
             self.__correo = correo.strip()
-
+            self.__telefono = telefono.strip()
+            
         except ClienteInvalidoError as e:
-            registrar_log(str(e))
+            registrar_log("ERROR", str(e), "clientes")
             print("Error:", e)
 
     # Getters
@@ -25,9 +29,12 @@ class Cliente:
 
     def get_correo(self):
         return self.__correo
+    
+    def get_telefono(self):
+        return self.__telefono
 
     # Mostrar información
     def mostrar_info(self):
-        return f"Cliente: {self.__nombre} | Correo: {self.__correo}"
+        return f"Cliente: {self.__nombre} | Correo: {self.__correo} | Telefono: {self.__telefono}"
     
     
--- main.py
+++ main.py
@@ -2,15 +2,22 @@ from clientes import Cliente
 
 # Prueba con cliente válido
 print("Creando cliente válido...")
-cliente1 = Cliente("Rocio", "rocio@test.com")
+cliente1 = Cliente("Rocio", "rocio@test.com", "3101234567")
 print(cliente1.mostrar_info())
 
 # Prueba con nombre vacío
 print("\nCreando cliente inválido...")
-cliente2 = Cliente("", "correo@test.com")
+cliente2 = Cliente("", "correo@test.com", "3101234567")
 
 # Prueba con correo vacío
 print("\nCreando cliente con correo inválido...")
-cliente3 = Cliente("Laura", "")
+cliente3 = Cliente("Laura", "", "3101234567")
+
+print("\nCreando cliente con telefono invalido...")
+cliente4 = Cliente("Carlos", "carlos@test.com", "123")
+
+print("\nCreando cliente con espacios...")
+cliente5 = Cliente("    Andrea    ", "andrea@test.com", "3209876543")
+print(cliente5.mostrar_info())
 
 
