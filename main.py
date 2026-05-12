66b6304fd4d0ceec52f5281ab53fed58b93b89f8
Rocio <rocioarredondogalviz110@gmail.com>
2026-05-12 12:59:51 -0500

    Se agregan simulaciones adicionales y actualización de logs

--- main.py
+++ main.py
@@ -20,4 +20,21 @@ print("\nCreando cliente con espacios...")
 cliente5 = Cliente("    Andrea    ", "andrea@test.com", "3209876543")
 print(cliente5.mostrar_info())
 
+print("\n===== PRUEBAS ADICIONALES =====")
 
+# Cliente válido
+cliente6 = Cliente("Miguel", "miguel@test.com", "3004567890")
+print(cliente6.mostrar_info())
+
+# Nombre inválido
+cliente7 = Cliente("", "ana@test.com", "3101234567")
+
+# Correo inválido
+cliente8 = Cliente("Carlos", "carlosemail.com", "3101234567")
+
+# Teléfono con letras
+cliente9 = Cliente("Laura", "laura@test.com", "310ABC4567")
+
+# Nombre con espacios
+cliente10 = Cliente("   Andrea   ", "andrea@test.com", "3209876543")
+print(cliente10.mostrar_info())
